ea64c90cb1f4d792dc852928579ac6cf0274fe6e
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2024-11-28 10:03:57 +0530

    initial commit

---
+++ EmployeeAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
