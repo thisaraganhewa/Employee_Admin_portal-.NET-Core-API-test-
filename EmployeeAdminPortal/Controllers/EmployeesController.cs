fb56f047efeeea85eefc6e53f058e169bb08b70c
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2024-11-28 12:21:41 +0530

    added employee controller and add a method to get all employees

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeAdminPortal.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+
+            var allEmployees = dbContext.Employees.ToList();
+
+            return Ok(allEmployees);
+
+        }
+    }
+}
